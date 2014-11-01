2fae2536fd586d8babcf996d639c41624e31aa9c
Faizan Shabbir <faizanshabbir@users.noreply.github.com>
2014-10-31 20:59:15 -0700

    Generate classification using random forests
    Fix data reading, remove data preprocessing, use trained random forest
    model to classify test data, output csv file

--- py-rf/digit_recognizer-rf.py
+++ py-rf/digit_recognizer-rf.py
@@ -1,7 +1,7 @@
 import csv
 import numpy as np
 from numpy import genfromtxt, savetxt
-from sklearn import RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier
  
 
 #Specify training data, testing data, and labeled data file locations
@@ -9,22 +9,13 @@ ifile  = open('../Data/train.csv', "r")
 tfile = open('../Data/test.csv',"r")
 
 data = genfromtxt(ifile,delimiter=',',dtype='f8')[1:]
-labels = [x[0] for x in dataset]
-train =[x[1] for x in dataset]
+labels = [x[0] for x in data]
+train =[x[1:] for x in data]
 test = genfromtxt(tfile,delimiter=',',dtype='f8')[1:]
 
-#Preprocessing -- Scale data
-X = np.array([[ 1., -1.,  2.],
-              [ 2.,  0.,  0.],
-          	  [ 0.,  1., -1.]])
-
-X_scaled = preprocessing.scale(X)
-
-print(X_scaled)
-
 #Train data
-rf = RandomForestClassifierm(n_estimators=100)
-rf.fit(train,target)
+rf = RandomForestClassifier(n_estimators=100)
+rf.fit(train,labels)
 
 #Classify
 classified = rf.predict(test)
